6a19af5d1040e3cb0956d4c22009da3fb8a352c5
Alfred <alfred.toussaint@epitech.eu>
2025-03-26 08:22:31 +0100

    Ajout

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import './App.css'
+
+const App = () => {
+  return (
+    <body className='bg-black items-center flex items-center justify-center'>
+      <div className='bg-gray-900 w-11/12 h-96 rounded-lg'>
+      <div>
+        <div className='flex flex-col items-center justify-center'>
+          <h1>TOUSSAINT</h1>
+          <h1>Alfred</h1>
+          <h2>Web Developper</h2>
+        </div>
+      </div>
+      </div>
+    </body>
+  );
+};
+
+export default App
