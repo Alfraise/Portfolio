241062111cc4dd5bb5b42b9d8a2a7d7b91eca249
Alfred <alfred.toussaint@epitech.eu>
2025-03-29 12:00:42 +0100

    Ajout

--- src/pages/acceuil.jsx
+++ src/pages/acceuil.jsx
@@ -1,6 +1,6 @@
 const Acceuil = () => {
     return (
-        <div id="Acceuil" className="flex flex-col justify-center items-center my-80 mx-190">
+        <div id="Acceuil" className="flex flex-col justify-center items-center mt-70 mb-90 mx-190 border-b-light-50">
             <h1>TOUSSAINT</h1>
             <h1>Alfred</h1>
             <h2>Web Developper</h2>
