9e5a4579733762aa00bd5518ea9701b69c7fc075
Alfred <alfred.toussaint@epitech.eu>
2025-04-11 11:22:38 +0200

    Correction affichage image

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -1,4 +1,5 @@
 import { Link } from "react-router-dom";
+import Fraise from "../assets/Fraise.png";
 
 
 const Navbar = () => {
@@ -8,7 +9,7 @@ const Navbar = () => {
       <ul className="flex justify-evenly w-1/5 h-16 space-x-8 items-center bg-gray-700 rounded-lg mx-10">
 
         <li className="hover:text-gray-400 m-0">
-          <Link to="/"><img className="rounded-full w-13" src="./public/Fraise.png" alt="Une Fraise" title="Une Fraise" /></Link>
+          <Link to="/"><img className="rounded-full w-13" src={Fraise} alt="Une Fraise" title="Une Fraise" /></Link>
         </li>
 
         <li className="hover:text-gray-400 m-0">
